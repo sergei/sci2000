f02a5292481c918de335bb4edf857d5b2d2da0d3
Sergei <spodshivalov@scmarinetech.com>
2023-01-07 17:16:43 -0800

    Started android project to do the calibration

---
+++ android/Calibrator/app/src/main/java/com/santacruzinstruments/scicalibrator/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.santacruzinstruments.scicalibrator.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
