b5ab52bb84bf4d538152d312b2f5e444870eecaf
Sergei <spodshivalov@scmarinetech.com>
2023-01-07 18:19:56 -0800

    GUI to set and display TWA

--- android/Calibrator/app/src/main/java/com/santacruzinstruments/scicalibrator/ui/main/MainFragment.java
+++ android/Calibrator/app/src/main/java/com/santacruzinstruments/scicalibrator/ui/main/MainFragment.java
@@ -11,11 +11,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.SeekBar;
 
 import com.santacruzinstruments.scicalibrator.R;
+import com.santacruzinstruments.scicalibrator.databinding.FragmentMainBinding;
 
 public class MainFragment extends Fragment {
 
+    private FragmentMainBinding binding;
     private MainViewModel mViewModel;
 
     public static MainFragment newInstance() {
@@ -26,14 +30,64 @@ public class MainFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
-        // TODO: Use the ViewModel
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        binding = FragmentMainBinding.inflate(inflater, container, false);
+
+        // TWA
+        binding.buttonTwa.setOnClickListener(view -> {
+            if( binding.seekBarTwa.getVisibility() == View.VISIBLE){
+                binding.seekBarTwa.setVisibility(View.GONE);
+                binding.buttonTwaCancel.setVisibility(View.GONE);
+                binding.twaCalValue.setVisibility(View.GONE);
+                ((Button)view).setText(R.string.calibrate);
+                mViewModel.submitTwaCal();
+            }else{
+                binding.seekBarTwa.setVisibility(View.VISIBLE);
+                binding.buttonTwaCancel.setVisibility(View.VISIBLE);
+                binding.twaCalValue.setVisibility(View.VISIBLE);
+                ((Button)view).setText(R.string.submit);
+            }
+        });
+        binding.buttonTwaCancel.setOnClickListener(view -> {
+            binding.seekBarTwa.setVisibility(View.GONE);
+            binding.buttonTwaCancel.setVisibility(View.GONE);
+            binding.buttonTwa.setText(R.string.calibrate);
+        });
+        binding.seekBarTwa.setMin(-45);
+        binding.seekBarTwa.setMax(45);
+        binding.seekBarTwa.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                mViewModel.setTwaCal(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        mViewModel.getTwaCal().observe(getViewLifecycleOwner(),
+                val -> binding.twaCalValue.setText(getString(R.string.cal_angle, val)));
+        mViewModel.getTwa().observe(getViewLifecycleOwner(),
+                val -> binding.twaValue.setText(val));
+
+        return binding.getRoot();
     }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 }
--- android/Calibrator/app/src/main/java/com/santacruzinstruments/scicalibrator/ui/main/MainViewModel.java
+++ android/Calibrator/app/src/main/java/com/santacruzinstruments/scicalibrator/ui/main/MainViewModel.java
@@ -1,7 +1,25 @@
 package com.santacruzinstruments.scicalibrator.ui.main;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    MutableLiveData<Integer> twaCal = new MutableLiveData<>(0);
+    MutableLiveData<String>  twa = new MutableLiveData<>("0=0+0");
+
+    LiveData<Integer> getTwaCal(){
+        return twaCal;
+    }
+
+    public void setTwaCal(int twaCal) {
+        this.twaCal.postValue(twaCal);
+    }
+    public void submitTwaCal() {
+    }
+
+    LiveData<String> getTwa(){
+        return twa;
+    }
+
 }
