0d99966c1ba655cdde226fb3544d5ade61cd18f6
Sergei <spodshivalov@scmarinetech.com>
2023-04-24 17:18:16 -0700

    Added N2K over UDP

---
+++ idf-components/N2K_WIFI/N2kWifi.h
@@ -0,0 +1,59 @@
+#ifndef IMU2NMEA_N2KWIFI_H
+#define IMU2NMEA_N2KWIFI_H
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "N2kMessages.h"
+#include "../NMEA2000_esp32_twai/NMEA2000_esp32_twai.h"
+enum NetworkMsgType {
+    CAN_FRAME
+    ,WIFI_CONNECTED
+    ,WIFI_DISCONNECTED
+};
+
+struct NetworkMsg{
+    NetworkMsgType type=CAN_FRAME;
+    unsigned long id = 0;
+    unsigned char len = 0;
+    unsigned char pdu[8] ={};
+};
+
+struct SsidPassList{
+    const char* ssid;
+    const char* password;
+};
+const int UDP_RX_PORT = 2023;
+const int UDP_TX_PORT = 2024;
+const int DEFAULT_SCAN_LIST_SIZE = 256;
+
+class N2kWifi : public TwaiBusListener {
+public:
+    N2kWifi(SideTwaiBusInterface &twaiBusSender);
+    void Start();
+    [[noreturn]] void TransmitFrameTask();
+    [[noreturn]] void ReceiveFrameTask();
+
+    void WifiEventHandler(int32_t event_id, void *event_data);
+    void onTwaiFrameReceived(unsigned long id, unsigned char len, const unsigned char *buf) override;
+    void onTwaiFrameTransmit(unsigned long id, unsigned char len, const unsigned char *buf) override;
+
+private:
+    static bool CheckScanResults(char *ssid, char *password);
+    void StartWifi();
+    void StartServer();
+    void StopServer();
+    void ListenForUdpInput();
+
+private:
+    SideTwaiBusInterface &twaiBusSender;
+    xQueueHandle txFrameQueue;
+    xQueueHandle rxFrameQueue;
+    bool volatile isWifiConnected = false;
+
+    void BroadcastCanFramesOverUdp();
+
+    static int CreateBoundUdpSocket();
+};
+
+
+#endif //IMU2NMEA_N2KWIFI_H
--- imu2nmea/main/imu2nmea_main.cpp
+++ imu2nmea/main/imu2nmea_main.cpp
@@ -1,3 +1,5 @@
+#include <N2kWifi.h>
+#include <nvs_flash.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/queue.h"
 #include "esp_log.h"
@@ -7,6 +9,7 @@
 #include "LEDBlinker.h"
 #include "GPSHandler.h"
 
+#define ENABLE_WIFI
 
 xQueueHandle evt_queue;   // A queue to handle  send events from sensors to N2K
 
@@ -19,6 +22,10 @@ IMUHandler imuHandler(evt_queue, SDA_IO_NUM, SCL_IO_NUM, CMPS12_i2C_ADDR);
 N2KHandler n2KHandler(evt_queue, ledBlinker);
 GPSHandler gpsHandler(evt_queue, 15, 13);
 
+#ifdef ENABLE_WIFI
+N2kWifi n2kWifi(n2KHandler);
+#endif
+
 static const char *TAG = "imu2nmea_main";
 
 extern "C" [[noreturn]] void app_main(void)
@@ -27,6 +34,20 @@ extern "C" [[noreturn]] void app_main(void)
 
     // Initialize event queue
     evt_queue = xQueueCreate(10, sizeof(Event));
+
+#ifdef ENABLE_WIFI
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    n2kWifi.Start();
+    n2KHandler.addBusListener(&n2kWifi);
+#endif
+
+
     ledBlinker.Start();
     n2KHandler.Start();
     gpsHandler.Start();
